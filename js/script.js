1f6050ba8a14def9948fb3c2fec2770ff6ada7a0
Dayn <danielbalmoria@gmail.com>
2025-08-10 15:35:45 +0800

    finalize stuff

--- js/script.js
+++ js/script.js
@@ -25,13 +25,49 @@ window.addEventListener('DOMContentLoaded', event => {
             let image1 = getCharInfo['faded_link'];
             let image2 = getCharInfo['full_link'];
 
-            MangoTemplateIconTemp = `<div class="col-md-4 col-lg-3 mb-4">
-                                        <img src="`+image1+`" class="img-fluid" alt="Responsive image">
-                                    </div>`;
+            QuestionTemp = `<div class="col-md-4 col-lg-3 mx-auto mb-4">
+                                        <div class="portfolio-item mx-auto"  >
+                                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                                <div class="portfolio-item-caption-content text-center text-white">
+                                            </div>
+                                            <img class="img-fluid rounded mb-5 center thumbnails" src="`+image1+`" alt="..." /> <br>
+                                            
+                                        </div>
+                                    </div>`
+
+             ModalTemp = `<div class="portfolio-modal modal fade" id="portfolioModal`+name+`" tabindex="-1" aria-labelledby="portfolioModal`+name+`" aria-hidden="true">
+                                    <div class="modal-dialog modal-xl">
+                                        <div class="modal-content">
+                                           
+                                            <div class="modal-body text-center pb-5">
+                                                <div class="container">
+                                                    <div class="row justify-content-center">
+                                                        <div class="col-lg-8">
+                                                            `+name+`
+                                                            <img class="img-fluid rounded mb-5 center thumbnails" src="`+image2+`" alt="..." />
+                                                            <button onClick="window.location.reload()" class="btn btn-primary">
+                                                                Next
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`;
+
+            ShowTemp = `<button href="#" data-bs-toggle="modal" class="btn btn-outline-primary" data-bs-target="#portfolioModal`+name+`">SHOW</a>
+                                            `
 
     
             const boxProper = document.getElementById('img-tarvet-div');  
-            boxProper.innerHTML = MangoTemplateIconTemp;
+            boxProper.innerHTML = QuestionTemp;
+
+            const boxModal = document.getElementById('modal-box');  
+            boxModal.innerHTML = ModalTemp;
+
+            const boxButton = document.getElementById('button-target');  
+            boxButton.innerHTML = ShowTemp;
         })
         
     }
